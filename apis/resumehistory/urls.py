26b7fff24d0b0b751a8dcac8142fe3ff58e8171d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-02-17 22:12:48 +0100

    Update urls.py

--- apis/resumehistory/urls.py
+++ apis/resumehistory/urls.py
@@ -1,7 +1,8 @@
-from .views import UploadResumeView, TailorResumeView
+from .views import UploadResumeView, TailorResumeView, GetHistoriesView
 from django.urls import path
 
 urlpatterns = [
     path("upload", UploadResumeView.as_view(), name="upload-resume"),
     path("tailor", TailorResumeView.as_view(), name="tailor-resume"),
+    path("histories", GetHistoriesView.as_view(), name="get-histories"),
 ]
