c3120e0a7456de40c3730d7060002a5af84c5478
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2026-06-03 16:07:30 +0100

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker resumeai_proj.asgi:application --timeout 120
+#gunicorn -w 4 -k uvicorn.workers.UvicornWorker resumeai_proj.asgi:application --timeout 120
+uvicorn resumeai_proj.asgi:application --reload --port 8001
